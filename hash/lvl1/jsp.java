18d45dab0c73f233bd3a5958d13b42d77a329990
scollector65 <scollector65@naver.com>
2019-09-16 23:41:45 +0900

    Create jsp.java

---
+++ hash/lvl1/jsp.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("run");
+		String[] participant = { "leo", "kiki", "eden" };
+		String[] completion = { "eden", "kiki" };
+		String solution = solution(participant, completion);
+		log.info("{}", solution);
+	}
+
+	public String solution(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		for (int i = 0; i < completion.length; i++) {
+			if (!participant[i].equals(completion[i])) {
+				return participant[i];
+			}
+		}
+		return participant[participant.length - 1];
+	}
+}
